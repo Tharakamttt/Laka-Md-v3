744f7e97325fb4e7c231547af671152bea91d302
Tharakamttt <Mihingutharaka05@gmail.com>
2025-05-30 08:59:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,3 +7,4 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Session id",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
+𝐋ᴀᴋᴀ-𝐌ᴅ=goRERDga#YZIJEtVY4Akn45mZsaGJFl9O6aaNbRCq5UibTLkgjXs
